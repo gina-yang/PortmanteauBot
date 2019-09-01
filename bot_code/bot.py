e5d4e442d47730c8adafe4991f4ad785fe6a1fbd
Gina Yang <ginayang@usc.edu>
2019-09-01 00:09:13 -0700

    Initial tweepy stuff complete

--- bot_code/bot.py
+++ bot_code/bot.py
@@ -1,11 +1,11 @@
 import tweepy
-import secrets
+from secrets import *
 import time
-import PortmanteauCreator
+from PortmanteauCreator import *
 
 # Twitter requires all requests to use OAth for authentication
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 # Construct the API instance
 api = tweepy.API(auth)
 
@@ -14,13 +14,11 @@ def handlePortmanteau(tweet_text):
 	word1 = tweet.split()[1]
 	word2 = tweet.split()[2]
 	portmanteau = PortmanteauCreator.portmanteau(word1, word2)
-	if portmanteau == "n/a":  # Somehow the input word(s) were invalid; don't tweet
+	if portmanteau == "n/a":  # Somehow the input word(s) were invalid; don't tweet anything
 		return
-	tweetPortmanteau(word1, word2, portmanteau)
-
-# Function to handle tweeting the completed portmanteau
-def tweetPortmanteau(word1, word2, portmanteau):
 	
+def tweetPortmanteau(status_id, word1, word2, portmanteau):
+	api.update_status(word1 + " + " + word2 + " = " + portmanteau)
 
 # A class inheriting from tweepy.StreamListener
 class BotStreamer(tweepy.StreamListener):
@@ -31,17 +29,18 @@ class BotStreamer(tweepy.StreamListener):
 		# Get tweet text, check formatting
 		tweet_text = status.text
 		if check_formatting(tweet_text):
-			handlePortmanteau(tweet_text)
+			portmanteau = handlePortmanteau(tweet_text)
+
 
 	def check_formatting(self, tweet):
 		if len(tweet.split()) == 3:  # Tweet length should be 3: @mention word1 word2
 			word1 = tweet.split()[1]
 			word2 = tweet.split()[2]
-			if word1.isalpha() && word2.isalpha():  # Words should only contain letters
+			if word1.isalpha() and word2.isalpha():  # Words should only contain letters
 				return true
 		return false
 
 myStreamListener = BotStreamer()
 # Construct Stream instance
 stream = tweepy.Stream(auth, myStreamListener)
-stream.filter(track=['@makeportmanteau'])
+stream.filter(track=['@makeportmanteau'])  # how BotStreamer.on_status is called
